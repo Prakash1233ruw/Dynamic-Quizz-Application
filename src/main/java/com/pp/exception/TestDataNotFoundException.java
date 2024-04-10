b26c9d75b3875dcf53f6f6d1f2d2d73268277286
prakash <pr0018908@gmail.com>
2024-04-10 13:42:56 +0530

    added  custom exceptions

---
+++ src/main/java/com/pp/exception/TestDataNotFoundException.java
@@ -0,0 +1,7 @@
+package com.pp.exception;
+
+public class TestDataNotFoundException extends RuntimeException {
+public TestDataNotFoundException(String message){
+		super(message);
+	}
+}
---
+++ src/main/java/com/pp/exception/TestResultsNotFoundException.java
@@ -0,0 +1,7 @@
+package com.pp.exception;
+
+public class TestResultsNotFoundException extends RuntimeException {
+public TestResultsNotFoundException(String message) {
+	super(message);
+}
+}
