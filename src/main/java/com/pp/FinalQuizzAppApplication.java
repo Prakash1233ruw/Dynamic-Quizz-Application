687ee1ccedde9ea64aa03d81216b846fa0ee764f
prakash <pr0018908@gmail.com>
2024-04-10 13:45:55 +0530

    main added

---
+++ src/main/java/com/pp/FinalQuizzAppApplication.java
@@ -0,0 +1,13 @@
+package com.pp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalQuizzAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalQuizzAppApplication.class, args);
+	}
+
+}
